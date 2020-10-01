b6638e2fbd841766adb5e974abf368b75a33d949
kichichoi102 <70384232+kichichoi102@users.noreply.github.com>
2020-10-01 12:44:54 -0400

    removed preload fxn

--- main.js
+++ main.js
@@ -2,35 +2,6 @@
 const connectBtn = document.querySelector(".connect");
 const arrow = document.querySelector(".arrow");
 const socialMediaList = document.querySelector(".social-media-list");
-const preloader = document.querySelector('.loader-wrapper');
-
-const fadeEffect = setInterval(() => {
-  // if we don't set opacity 1 in CSS, then
-  // it will be equaled to "" -- that's why
-  // we check it, and if so, set opacity to 1
-  if (!preloader.style.opacity) {
-    preloader.style.opacity = 3;
-  }
-  if (preloader.style.opaccity == 0.1) {
-    document.getElementById("loader-wrapper").remove();
-  }
-  if (preloader.style.opacity > 0) {
-    preloader.style.opacity -= 0.1;
-
-    if (preloader.style.opacity == 0) {
-        preloader.style.visibility = "hidden";
-    }
-
-
-  }
-
-  else {
-    clearInterval(fadeEffect);
-  }
-}, 100);
-
-
-window.addEventListener('load', fadeEffect);
 
 connectBtn.addEventListener("click",()=>{
     if (!socialMediaList.classList.contains("active")){
